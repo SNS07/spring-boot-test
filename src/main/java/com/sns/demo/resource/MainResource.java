22a36e279336c1a61486d8c2347d730679d4e99e
Sreenath Sasidharan <sreenaths.s9@gmail.com>
2019-10-20 16:33:16 +0530

    Initial commit

---
+++ src/main/java/com/sns/demo/resource/MainResource.java
@@ -0,0 +1,16 @@
+package com.sns.demo.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("main/test")
+public class MainResource {
+	
+	@GetMapping
+	public  String test() {
+		return "Hello Kuernetes";
+	}
+
+}
